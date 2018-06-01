9c1280b7d665bd6c8b17bbbf1f1364b721be6e1a
kota miyasaka <km.cool.desire@gmail.com>
2018-06-01 12:31:18 +0900

    トランザクション管理用のカラムを各テーブルに追加

--- app/models/entity/TbWorkParam.java
+++ app/models/entity/TbWorkParam.java
@@ -4,6 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Version;
+import java.sql.Timestamp;
 
 import play.db.ebean.Model;
 
