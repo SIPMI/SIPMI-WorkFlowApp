a43f60c2cca1cdeeb823254b9ae707bf1b84843c
Yukihiro Ito <c5617130@aoyama.jp>
2018-12-07 17:15:44 +0900

    reverse

--- app/controllers/TaskRunner.java
+++ app/controllers/TaskRunner.java
@@ -282,154 +282,155 @@ try {
 
 //#end#3,4
 	);
-}
 					break;
 
 
-	    	case "Visualize":
-	    	case "RegistVRApp":
-	    		// 結果が自分のワークのパラメータに保存されているので
-	    		// ここでは何もしない
-	    		break;
+						    	case "Visualize":
+						    	case "RegistVRApp":
+						    		// 結果が自分のワークのパラメータに保存されているので
+						    		// ここでは何もしない
+						    		break;
 
-	    	default:
+						    	default:
 
-	    		if(work.task.startsWith("InputData")){
-	    			// 親のワークのパラメータに保存
-		    		TbWorkParamDao.registWorkParam(work.parentId, 1, "InputData", paramList.get(0).paramText);
+						    		if(work.task.startsWith("InputData")){
+						    			// 親のワークのパラメータに保存
+							    		TbWorkParamDao.registWorkParam(work.parentId, 1, "InputData", paramList.get(0).paramText);
 
-	    		}else{
+						    		}else{
 
-		    		//ファンクションの取得
-		    		TbFunction func = TbFunctionDao.findFunctionByTypeName(work.task);
-		    		if(func == null){
-		    			Logger.info("function not found !!");
-		    			break;
-		    		}
+							    		//ファンクションの取得
+							    		TbFunction func = TbFunctionDao.findFunctionByTypeName(work.task);
+							    		if(func == null){
+							    			Logger.info("function not found !!");
+							    			break;
+							    		}
 
-		    		//リモートのファンクション呼び出し
-		    		//String url = "http://localhost:9000/convertGrayScale";
-		    		String url = func.url;
-		    		String req = paramList.get(0).paramText;
-		    		ObjectNode node = Json.newObject();
-		    		node.put("img", paramList.get(0).paramText);
-		    		if(paramList.size() > 1){
-		    			node.put("threshold", paramList.get(1).paramText);
-		    		}
+							    		//リモートのファンクション呼び出し
+							    		//String url = "http://localhost:9000/convertGrayScale";
+							    		String url = func.url;
+							    		String req = paramList.get(0).paramText;
+							    		ObjectNode node = Json.newObject();
+							    		node.put("img", paramList.get(0).paramText);
+							    		if(paramList.size() > 1){
+							    			node.put("threshold", paramList.get(1).paramText);
+							    		}
 
-		    		Promise<WS.Response> response = WS.url(url)
-		    											//.setQueryParameter("img", req)
-		    											//.post(Json.newObject().put("img", req));
-		    											.post(node);
+							    		Promise<WS.Response> response = WS.url(url)
+							    											//.setQueryParameter("img", req)
+							    											//.post(Json.newObject().put("img", req));
+							    											.post(node);
 
-		        	// 親のワークのパラメータに保存
-		    		TbWorkParamDao.registWorkParam(work.parentId, 1, "Remote Function for Visualize", response.get().asJson().findValue("img").asText());
+							        	// 親のワークのパラメータに保存
+							    		TbWorkParamDao.registWorkParam(work.parentId, 1, "Remote Function for Visualize", response.get().asJson().findValue("img").asText());
 
-	    		}
-	    	}
+						    		}
+						    	}
 
+					    }
 
+					    // ダミー判断
+					    private Boolean isDummy(List<TbWork> workList){
 
-    // ダミー判断
-    private Boolean isDummy(List<TbWork> workList){
+					    	Boolean isDummy = false;
 
-    	Boolean isDummy = false;
+					    	for(TbWork work : workList){
+					    		// controls_whileUntilが含まれていたらダミーと判断
+					    		if(work.task.equals("controls_whileUntil")){
+					    			isDummy = true;
+					    			break;
+					        	}
+					    	}
 
-    	for(TbWork work : workList){
-    		// controls_whileUntilが含まれていたらダミーと判断
-    		if(work.task.equals("controls_whileUntil")){
-    			isDummy = true;
-    			break;
-        	}
-    	}
+					    	return isDummy;
 
-    	return isDummy;
+					    }
 
-    }
+					    private void executeDummyLogic(List<TbWork> workList){
 
-    private void executeDummyLogic(List<TbWork> workList){
+					    	Logger.info("execute DummyLogic");
 
-    	Logger.info("execute DummyLogic");
+					    	// 一旦初期値をセット
+					    	Double i = 100.0;
+					    	Double iPlus = 1.0;
+					    	Double average = 200.0;
+					    	Double averageUntil = 255.0;
+					    	String inputData = "";
+					    	String tmp_img = "";
 
-    	// 一旦初期値をセット
-    	Double i = 100.0;
-    	Double iPlus = 1.0;
-    	Double average = 200.0;
-    	Double averageUntil = 255.0;
-    	String inputData = "";
-    	String tmp_img = "";
+					    	for(TbWork work : workList){
+					    		if(work.task.equals("InputData")){
+					    			List<TbWorkParam> paramList = TbWorkParamDao.findWorkParamListByWorkId(work.id.longValue());
+					    			inputData = paramList.get(0).paramText;
+					    		}else if(work.task.equals("controls_whileUntil")){
+					    			List<TbWorkParam> paramList = TbWorkParamDao.findWorkParamListByWorkId(work.id.longValue());
+					    			i = Double.valueOf(paramList.get(0).paramText);
+					    			average = Double.valueOf(paramList.get(1).paramText);
+					    			averageUntil = Double.valueOf(paramList.get(2).paramText);
+					    			iPlus = Double.valueOf(paramList.get(3).paramText);
+					    		}
+					    	}
 
-    	for(TbWork work : workList){
-    		if(work.task.equals("InputData")){
-    			List<TbWorkParam> paramList = TbWorkParamDao.findWorkParamListByWorkId(work.id.longValue());
-    			inputData = paramList.get(0).paramText;
-    		}else if(work.task.equals("controls_whileUntil")){
-    			List<TbWorkParam> paramList = TbWorkParamDao.findWorkParamListByWorkId(work.id.longValue());
-    			i = Double.valueOf(paramList.get(0).paramText);
-    			average = Double.valueOf(paramList.get(1).paramText);
-    			averageUntil = Double.valueOf(paramList.get(2).paramText);
-    			iPlus = Double.valueOf(paramList.get(3).paramText);
-    		}
-    	}
+					    	Integer workflowId = 0;
+					    	// 一旦削除して以下で入れ直す
+					    	for(TbWork work : workList){
+					    		if(work.sortNo != 1){
+					    			work.delete();
+					    		}else{
+					    			workflowId = work.workflowId;
+					    		}
+					    	}
 
-    	Integer workflowId = 0;
-    	// 一旦削除して以下で入れ直す
-    	for(TbWork work : workList){
-    		if(work.sortNo != 1){
-    			work.delete();
-    		}else{
-    			workflowId = work.workflowId;
-    		}
-    	}
+					    	Integer sortNo = 2;
+					    	List<Long> workIdList;
 
-    	Integer sortNo = 2;
-    	List<Long> workIdList;
+					    	while(! (average < averageUntil)){
+					    		Logger.info("i: " + i);
+					    		workIdList = new ArrayList<Long>();
 
-    	while(! (average < averageUntil)){
-    		Logger.info("i: " + i);
-    		workIdList = new ArrayList<Long>();
+					    		workIdList.add(TbWorkDao.registWork("InputData", sortNo++, null, workflowId).longValue());
+					    		workIdList.add(TbWorkDao.registWork("Binarization", sortNo++, null, workflowId).longValue());
+					    		workIdList.add(TbWorkDao.registWork("Visualize", sortNo++, null, workflowId).longValue());
+					    		workIdList.add(TbWorkDao.registWork("CalcAverageBrightness", sortNo++, null, workflowId).longValue());
+					    		workIdList.add(TbWorkDao.registWork("OutputData", sortNo++, null, workflowId).longValue());
 
-    		workIdList.add(TbWorkDao.registWork("InputData", sortNo++, null, workflowId).longValue());
-    		workIdList.add(TbWorkDao.registWork("Binarization", sortNo++, null, workflowId).longValue());
-    		workIdList.add(TbWorkDao.registWork("Visualize", sortNo++, null, workflowId).longValue());
-    		workIdList.add(TbWorkDao.registWork("CalcAverageBrightness", sortNo++, null, workflowId).longValue());
-    		workIdList.add(TbWorkDao.registWork("OutputData", sortNo++, null, workflowId).longValue());
+					    		// inputData
 
-    		// inputData
+					    		TbWorkDao.updateForExecute(TbWorkDao.findWorkById(workIdList.get(0)));
+					    		TbWorkParamDao.registWorkParam(workIdList.get(0).intValue(), 1, "InputData", inputData);
+					    		TbWorkDao.updateForSuccess(TbWorkDao.findWorkById(workIdList.get(0)));
 
-    		TbWorkDao.updateForExecute(TbWorkDao.findWorkById(workIdList.get(0)));
-    		TbWorkParamDao.registWorkParam(workIdList.get(0).intValue(), 1, "InputData", inputData);
-    		TbWorkDao.updateForSuccess(TbWorkDao.findWorkById(workIdList.get(0)));
+					    		// binarize
+					    		TbWorkDao.updateForExecute(TbWorkDao.findWorkById(workIdList.get(1)));
+					    		tmp_img = ImageUtil.binarize(inputData, i);
+					    		TbWorkDao.updateForSuccess(TbWorkDao.findWorkById(workIdList.get(1)));
 
-    		// binarize
-    		TbWorkDao.updateForExecute(TbWorkDao.findWorkById(workIdList.get(1)));
-    		tmp_img = ImageUtil.binarize(inputData, i);
-    		TbWorkDao.updateForSuccess(TbWorkDao.findWorkById(workIdList.get(1)));
+					    		// Visualize にセット
+					    		TbWorkDao.updateForExecute(TbWorkDao.findWorkById(workIdList.get(2)));
+					    		TbWorkParamDao.registWorkParam(workIdList.get(2).intValue(), 1, "Visualize", tmp_img);
+					    		TbWorkDao.updateForSuccess(TbWorkDao.findWorkById(workIdList.get(2)));
 
-    		// Visualize にセット
-    		TbWorkDao.updateForExecute(TbWorkDao.findWorkById(workIdList.get(2)));
-    		TbWorkParamDao.registWorkParam(workIdList.get(2).intValue(), 1, "Visualize", tmp_img);
-    		TbWorkDao.updateForSuccess(TbWorkDao.findWorkById(workIdList.get(2)));
+					    		// calcAverageBrightness
+					    		TbWorkDao.updateForExecute(TbWorkDao.findWorkById(workIdList.get(3)));
+					    		average = Double.valueOf(ImageUtil.calcAverageBrightness(tmp_img));
+					    		Logger.info("average: " + average);
+					    		TbWorkDao.updateForSuccess(TbWorkDao.findWorkById(workIdList.get(3)));
 
-    		// calcAverageBrightness
-    		TbWorkDao.updateForExecute(TbWorkDao.findWorkById(workIdList.get(3)));
-    		average = Double.valueOf(ImageUtil.calcAverageBrightness(tmp_img));
-    		Logger.info("average: " + average);
-    		TbWorkDao.updateForSuccess(TbWorkDao.findWorkById(workIdList.get(3)));
-
-    		// OutputData にセット
-    		TbWorkDao.updateForExecute(TbWorkDao.findWorkById(workIdList.get(4)));
-    		TbWorkParamDao.registWorkParam(workIdList.get(4).intValue(), 1, "OutputData", average.toString());
-    		TbWorkDao.updateForSuccess(TbWorkDao.findWorkById(workIdList.get(4)));
-
-
-
-    		i = i + iPlus;
-    		tmp_img = null;
-    		workIdList = null;
-    	}
+					    		// OutputData にセット
+					    		TbWorkDao.updateForExecute(TbWorkDao.findWorkById(workIdList.get(4)));
+					    		TbWorkParamDao.registWorkParam(workIdList.get(4).intValue(), 1, "OutputData", average.toString());
+					    		TbWorkDao.updateForSuccess(TbWorkDao.findWorkById(workIdList.get(4)));
 
 
 
-    }
-}
+					    		i = i + iPlus;
+					    		tmp_img = null;
+					    		workIdList = null;
+					    	}
+
+
+
+					    }
+
+
+					}
